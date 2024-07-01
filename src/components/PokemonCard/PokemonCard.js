28f4c3c531aa0a7d4016febb8c2e2e7b91aea9d6
stepweaver <weaverst@mail.gvsu.edu>
2024-07-01 12:09:47 -0400

    Refactor PokemonCard component and add CSS class for flipping card

--- src/components/PokemonCard/PokemonCard.js
+++ src/components/PokemonCard/PokemonCard.js
@@ -1,8 +1,10 @@
 import React, { useState } from 'react';
 import { catchPokemon, releasePokemon } from '../../services/catchPokemonService';
+import './PokemonCard.css';
 
 const PokemonCard = ({ pokemon }) => {
   const [isCaught, setIsCaught] = useState(false);
+  const [isFlipped, setIsFlipped] = useState(false);
 
   const handleCatchChange = async () => {
     setIsCaught(!isCaught);
@@ -13,24 +15,29 @@ const PokemonCard = ({ pokemon }) => {
     }
   };
 
+  const flipCard = () => {
+    setIsFlipped(!isFlipped);
+  };
+
   return (
-    <div className="pokemon-card">
-      <div className="pokemon-header">
+    <div className={`pokemon-card ${isFlipped ? 'flipped' : ''}`} onClick={flipCard}>
+      <div className='pokemon-card-front'>
         <img src={pokemon.image} alt={pokemon.name} />
         <h2>{pokemon.name}</h2>
-        <input
-          type="checkbox"
-          checked={isCaught}
-          onChange={handleCatchChange}
-          id={`catch-${pokemon.name}`}
-        />
-        <label htmlFor={`catch-${pokemon.name}`}>{isCaught ? 'Caught' : 'Catch'}</label>
+        <div className='pokemon-types-container'>
+          {pokemon.types.map((type, index) => (
+            <span key={index} className={`pokemon-types ${type.toLowerCase().trim()}`}>
+              {type}
+            </span>
+          ))}
+        </div>
       </div>
-      <div className="pokemon-details">
-        <p>Types - {pokemon.types.join(", ")}</p>
+      <div className='pokemon-card-back'>
         <p>hp - {pokemon.hp}</p>
         <p>Attack - {pokemon.attack}</p>
         <p>Defense - {pokemon.defense}</p>
+        <input type='checkbox' checked={isCaught} onChange={handleCatchChange} id={`catch-${pokemon.name}`} />
+        <label htmlFor={`catch-${pokemon.name}`}>{isCaught ? 'Caught' : 'Catch'}</label>
       </div>
     </div>
   );
