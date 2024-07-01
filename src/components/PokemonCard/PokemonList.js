b6e7cc32425317d3ff8705ff7099f4790a19df92
stepweaver <weaverst@mail.gvsu.edu>
2024-07-01 14:58:43 -0400

    Use index from map function for unique key in PokemonList

--- src/components/PokemonCard/PokemonList.js
+++ src/components/PokemonCard/PokemonList.js
@@ -1,11 +1,12 @@
+// Corrected version: Use the index from the map function for a unique key
 import React from 'react';
 import PokemonCard from './PokemonCard';
 
 const PokemonList = ({ pokemons }) => {
   return (
     <div className="pokemon-list">
-      {pokemons.map(pokemon => (
-        <PokemonCard key={pokemon.id} pokemon={pokemon} />
+      {pokemons.map((pokemon, index) => (
+        <PokemonCard key={pokemon.id || index} pokemon={pokemon} />
       ))}
     </div>
   );
