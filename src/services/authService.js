6701b80b89c254b8171d010095603fb7a5d526bd
stepweaver <weaverst@mail.gvsu.edu>
2024-07-07 08:04:06 -0400

    Combined register and login auth services

---
+++ src/services/authService.js
@@ -0,0 +1,50 @@
+import Parse from 'parse';
+
+// used in auth register component
+export const createUser = (newUser) => {
+  const user = new Parse.User();
+
+  user.set('username', newUser.username);
+  user.set('email', newUser.email);
+  user.set('password', newUser.password);
+
+  console.log('User: ', user);
+  return user.signUp()
+    .then((newUserSaved) => {
+      return newUserSaved;
+    })
+    .catch((error) => {
+      alert(`Error: ${error.message}`);
+    });
+};
+
+// used in auth login component
+export const loginUser = (currUser) => {
+  return Parse.User.logIn(currUser.username, currUser.password)
+    .then((user) => {
+      console.log('Logged in user: ', user);
+      return user;
+    })
+    .catch((error) => {
+      alert(`Error: ${error.message}`);
+    });
+};
+
+// used in auth logout component
+export const logoutUser = (onLogout) => {
+  Parse.User.logOut()
+    .then(() => {
+      localStorage.clear();
+      if (typeof onLogout === 'function') {
+        onLogout();
+      }
+    })
+    .catch((error) => {
+      alert(`Error during logout: ${error.message}`);
+    });
+};
+
+export const checkUser = () => {
+  const currentUser = Parse.User.current();
+  return currentUser ? currentUser.authenticated() : false;
+};
