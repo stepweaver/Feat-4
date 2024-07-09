633986d454840e88963eb71691965c6c725c3a93
panchosepq19 <62320070+panchosepq19@users.noreply.github.com>
2024-07-09 05:33:53 -0400

    Update PokemonCard.js

--- src/components/PokemonCard/PokemonCard.js
+++ src/components/PokemonCard/PokemonCard.js
@@ -1,13 +1,22 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from "react";
 import {
   catchPokemon,
-  releasePokemon
-} from '../../Services/catchPokemonService';
-import './PokemonCard.css';
+  releasePokemon,
+} from "../../Services/catchPokemonService";
+import "./PokemonCard.css";
 
 const PokemonCard = ({ pokemon }) => {
   const [isCaught, setIsCaught] = useState(false);
   const [isFlipped, setIsFlipped] = useState(false);
+
+  useEffect(() => {
+    const caughtPokemon =
+      JSON.parse(localStorage.getItem("caughtPokemon")) || [];
+    const isPokemonCaught = caughtPokemon.some(
+      (p) => p.objectId === pokemon.objectId
+    );
+    setIsCaught(isPokemonCaught);
+  }, [pokemon]);
 
   const handleCatchChange = async () => {
     setIsCaught(!isCaught);
@@ -24,14 +33,14 @@ const PokemonCard = ({ pokemon }) => {
 
   return (
     <div
-      className={`pokemon-card ${isFlipped ? 'flipped' : ''}`}
+      className={`pokemon-card ${isFlipped ? "flipped" : ""}`}
       onClick={flipCard}
     >
       <img src={pokemon.image} alt={pokemon.name} />
       <h2>{pokemon.name}</h2>
-      <div className={isFlipped ? 'content flipped' : 'content'}>
-        <div className='pokemon-card-front'>
-          <div className='pokemon-types-container'>
+      <div className={isFlipped ? "content flipped" : "content"}>
+        <div className="pokemon-card-front">
+          <div className="pokemon-types-container">
             {pokemon.types.map((type, index) => (
               <span
                 key={index}
@@ -42,18 +51,18 @@ const PokemonCard = ({ pokemon }) => {
             ))}
           </div>
         </div>
-        <div className='pokemon-card-back'>
+        <div className="pokemon-card-back">
           <p>hp - {pokemon.hp}</p>
           <p>Attack - {pokemon.attack}</p>
           <p>Defense - {pokemon.defense}</p>
           <input
-            type='checkbox'
+            type="checkbox"
             checked={isCaught}
             onChange={handleCatchChange}
             id={`catch-${pokemon.name}`}
           />
           <label htmlFor={`catch-${pokemon.name}`}>
-            {isCaught ? 'Caught' : 'Catch'}
+            {isCaught ? "Caught" : "Catch"}
           </label>
         </div>
       </div>
@@ -63,4 +72,3 @@ const PokemonCard = ({ pokemon }) => {
 
 export default PokemonCard;
 
-// TODO: 'Caught' status doesn't persist after refreshing page. Need to make status persist for the logged in user.
