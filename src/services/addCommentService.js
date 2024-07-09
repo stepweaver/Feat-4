7d9e36ca089909f600823470dcb6320ec981769c
panchosepq19 <62320070+panchosepq19@users.noreply.github.com>
2024-07-09 06:17:00 -0400

    Update addCommentService.js
    Comments to addCommentService.js

--- src/services/addCommentService.js
+++ src/services/addCommentService.js
@@ -1,16 +1,17 @@
-import Parse from 'parse';
+import Parse from "parse";
 
 const addComment = async (commentText) => {
-  const Comment = new Parse.Object('Comment');
-  Comment.set('comment', commentText);
-  Comment.set('user', Parse.User.current());
+  const Comment = new Parse.Object("Comment"); // Create a new Parse Object for 'Comment'
+  Comment.set("comment", commentText); // Set the comment text
+  Comment.set("user", Parse.User.current()); // Set the current user
+
   try {
-    await Comment.save();
-    return true;
+    await Comment.save(); // Save the comment object to the Parse server
+    return true; // Return true if save is successful
   } catch (error) {
-    console.error('Error while creating comment: ', error);
-    return false;
+    console.error("Error while creating comment: ", error); // Log any errors
+    return false; // Return false if there is an error
   }
 };
 
-export { addComment };
+export { addComment }; // Export the addComment function
