5f4c43c2f95e5661653d5a9b34316ea577815e8e
stepweaver <weaverst@mail.gvsu.edu>
2024-07-16 09:49:33 -0400

    Rename branch.
    Rename function.
    
    Rename PokemonCard > Pokemon
    
    Refactor friendsService. FriendsList is rendering. Yay!

--- src/services/friendsService.js
+++ src/services/friendsService.js
@@ -1,31 +1,12 @@
 import Parse from 'parse';
 
-export const getFriendsList = async () => {
-  const currentUser = Parse.User.current();
-  if (!currentUser) {
-    console.error('No user logged in');
-    return [];
-  }
-
-  try {
-    const profileQuery = new Parse.Query('Profile');
-    profileQuery.equalTo('user', currentUser);
-    const userProfile = await profileQuery.first();
-    if (!userProfile) {
-      console.error('User profile not found');
-      return [];
-    }
-
-    const friendsList = userProfile.get('friendsList');
-    const friendsQuery = new Parse.Query(Parse.User);
-    friendsQuery.containedIn('objectId', friendsList);
-    const friends = await friendsQuery.find();
-    return friends.map(friend => ({
-      id: friend.id,
-      username: friend.get('username'),
-    }))
-  } catch (error) {
-    console.error('Error fetching friends list:', error);
-    return [];
-  }
+export const getAllFriends = () => {
+  const User = Parse.Object.extend('User');
+  const query = new Parse.Query(User);
+  return query.find().then((results) => {
+    return results.map((user) => ({
+      id: user.id,
+      username: user.get('username'),
+    }));
+  });
 }
