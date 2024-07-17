f3add9e986980ce970ca62d7845cff075c1db760
panchosepq19 <62320070+panchosepq19@users.noreply.github.com>
2024-07-17 06:22:27 -0400

    Update Register.js
    changes for landing page

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,56 +1,43 @@
-import React, { useEffect, useState } from "react";
-import { createUser, checkUser } from "../../Services/authService"; 
-import RegisterForm from "./RegisterForm"; 
-import { useNavigate } from "react-router-dom"; 
+import React, { useContext, useState } from "react";
+import { AuthContext } from "../../Context/AuthContext";
+import RegisterForm from "./RegisterForm";
+import { useNavigate } from "react-router-dom";
+import { createUser } from "../../Services/authService"; 
 
 const Register = () => {
-  const navigate = useNavigate(); // Hook to navigate programmatically
+  const { login } = useContext(AuthContext);
+  const navigate = useNavigate();
 
   const [newUser, setNewUser] = useState({
     firstName: "",
     lastName: "",
     email: "",
     password: "",
-  }); // State to store new user details
-
-  const [isAuthenticated, setIsAuthenticated] = useState(false); // State to track authentication status
-
-  useEffect(() => {
-    if (checkUser()) {
-      alert("You are already logged in");
-      navigate("/"); // Navigate to the home page if already logged in
-    }
-  }, [navigate]);
-
-  useEffect(() => {
-    if (newUser && isAuthenticated) {
-      createUser(newUser).then((userCreated) => {
-        if (userCreated) {
-          alert(
-            `${userCreated.get("firstName")} ${userCreated.get(
-              "lastName"
-            )} has been registered successfully!`
-          );
-          navigate("/"); // Navigate to the home page after successful registration
-        }
-        setIsAuthenticated(false); // Reset authentication status
-      });
-    }
-  }, [newUser, isAuthenticated, navigate]);
+  });
 
   const handleChange = (e) => {
     e.preventDefault();
-    const { name, value: newValue } = e.target;
-
+    const { name, value } = e.target;
     setNewUser({
       ...newUser,
-      [name]: newValue,
-    }); // Update new user state with form input values
+      [name]: value,
+    });
   };
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    setIsAuthenticated(true); // Set authentication status to true to trigger user creation
+    console.log("Registering user:", newUser); // Log the new user details
+    const userRegistered = await createUser(newUser);
+    if (userRegistered) {
+      alert(
+        `${userRegistered.get("firstName")} ${userRegistered.get(
+          "lastName"
+        )} has been registered successfully!`
+      );
+      login({ email: newUser.email, password: newUser.password }).then(() => {
+        navigate("/main");
+      });
+    }
   };
 
   return (
@@ -59,10 +46,9 @@ const Register = () => {
         user={newUser}
         onChange={handleChange}
         onSubmit={handleSubmit}
-      />{" "}
-      {/* Render the registration form */}
+      />
     </div>
   );
 };
 
-export default Register; // Export the Register component
+export default Register;
