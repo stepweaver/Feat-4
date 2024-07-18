b24567e3c04cf7fe180484a7a7e421fdf94d8bf3
panchosepq19 <62320070+panchosepq19@users.noreply.github.com>
2024-07-18 05:38:01 -0400

    Update App.js
    changes to deal with fetching and cleaning pokemon

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Components from "./Components/Components";
 import * as ENV from "./environments";
 import Parse from "parse";
 import "./index.css";
 import AuthProvider from "./Context/AuthContext";
+import { fetchAndSavePokemonData } from "./Services/getPokemonService";
+import { cleanDuplicatePokemon } from "./Services/cleanPokemonService";
 
 Parse.initialize(ENV.APPLICATION_ID, ENV.JAVASCRIPT_KEY);
 Parse.serverURL = ENV.SERVER_URL;
 
 const App = () => {
+  useEffect(() => {
+    fetchAndSavePokemonData();
+    cleanDuplicatePokemon();
+  }, []);
+
   return (
     <AuthProvider>
       <Components />
@@ -17,4 +24,5 @@ const App = () => {
 };
 
 export default App;
+
 
