db8da35283fdf310c97f92d090fbdac904360d7c
panchosepq19 <62320070+panchosepq19@users.noreply.github.com>
2024-07-18 05:11:20 -0400

    Update App.js
    changes towards loading more pokemon using API

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Components from "./Components/Components";
 import * as ENV from "./environments";
 import Parse from "parse";
 import "./index.css";
 import AuthProvider from "./Context/AuthContext";
+import { fetchAndSavePokemonData } from "./Services/getPokemonService";
 
 Parse.initialize(ENV.APPLICATION_ID, ENV.JAVASCRIPT_KEY);
 Parse.serverURL = ENV.SERVER_URL;
 
 const App = () => {
+  useEffect(() => {
+    fetchAndSavePokemonData();
+  }, []);
+
   return (
     <AuthProvider>
       <Components />
@@ -17,4 +22,3 @@ const App = () => {
 };
 
 export default App;
-
