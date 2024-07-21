70353fc7f8c34f01feb64e867e7cda06da1040f2
stepweaver <weaverst@mail.gvsu.edu>
2024-07-21 08:15:33 -0400

    Debug Friends List.
    Merge conflist with getPokemonService. Working now!

---
+++ src/services/cleanPokemonService.js
@@ -0,0 +1,45 @@
+import Parse from "parse";
+
+// Function to clean duplicate Pokemon entries
+export const cleanDuplicatePokemon = async () => {
+  try {
+    const Pokemon = Parse.Object.extend("Pokemon");
+    const seen = new Set();
+    let duplicates = [];
+    let hasMore = true;
+    let skip = 0;
+    const limit = 500; // Adjust
+
+    while (hasMore) {
+      const query = new Parse.Query(Pokemon);
+      query.ascending("number");
+      query.skip(skip);
+      query.limit(limit);
+
+      const results = await query.find();
+      if (results.length < limit) {
+        hasMore = false;
+      }
+
+      results.forEach((pokemon) => {
+        const number = pokemon.get("number");
+        if (seen.has(number)) {
+          duplicates.push(pokemon);
+        } else {
+          seen.add(number);
+        }
+      });
+
+      skip += limit;
+    }
+
+    if (duplicates.length > 0) {
+      await Parse.Object.destroyAll(duplicates);
+      console.log(`Deleted ${duplicates.length} duplicate Pokémon`);
+    } else {
+      // console.log("No duplicate Pokémon found");
+    }
+  } catch (error) {
+    console.error("Error cleaning duplicate Pokémon:", error);
+  }
+};
--- src/services/getPokemonService.js
+++ src/services/getPokemonService.js
@@ -1,7 +1,66 @@
 import Parse from 'parse';
 
-export const getAllPokemon = () => {
-  const Pokemon = Parse.Object.extend('Pokemon');
+export const fetchAndSavePokemonData = async () => {
+  const promises = [];
+  for (let i = 1; i <= 400; i++) {
+    const url = `https://pokeapi.co/api/v2/pokemon/${i}`;
+    promises.push(fetch(url).then((res) => res.json()));
+  }
+
+  const pokemonData = await Promise.all(promises);
+
+  const pokemonInfo = pokemonData.map((pokemon) => {
+    const stats = pokemon.stats.reduce((acc, stat) => {
+      acc[stat.stat.name] = stat.base_stat;
+      return acc;
+    }, {});
+
+    return {
+      number: pokemon.id.toString(),
+      name: pokemon.name,
+      imageURL: pokemon.sprites.front_default,
+      types: pokemon.types.map((typeInfo) => typeInfo.type.name),
+      hp: stats.hp.toString(),
+      attack: stats.attack.toString(),
+      defense: stats.defense.toString(),
+      specialAttack: stats["special-attack"].toString(),
+      specialDefense: stats["special-defense"].toString(),
+      speed: stats.speed.toString(),
+    };
+  });
+
+  try {
+    for (let info of pokemonInfo) {
+      const query = new Parse.Query("Pokemon");
+      query.equalTo("number", info.number);
+      const existingPokemon = await query.first();
+      if (!existingPokemon) {
+        const Pokemon = Parse.Object.extend("Pokemon");
+        const newPokemon = new Pokemon();
+        newPokemon.set("number", info.number);
+        newPokemon.set("name", info.name);
+        newPokemon.set("imageURL", info.imageURL);
+        newPokemon.set("types", info.types);
+        newPokemon.set("hp", info.hp);
+        newPokemon.set("attack", info.attack);
+        newPokemon.set("defense", info.defense);
+        newPokemon.set("specialAttack", info.specialAttack);
+        newPokemon.set("specialDefense", info.specialDefense);
+        newPokemon.set("speed", info.speed);
+        await newPokemon.save();
+        console.log(`Pokemon ${info.name} saved successfully`);
+      } else {
+        // console.table(`Pokemon ${info.name} already exists`);
+      }
+    }
+    console.log("Pokemon data fetched and saved successfully");
+  } catch (error) {
+    console.error("Error saving Pokémon data:", error);
+  }
+};
+
+export const getAllPokemon = async () => {
+  const Pokemon = Parse.Object.extend("Pokemon");
   const query = new Parse.Query(Pokemon);
   return query.find().then((results) => {
     return results.map((pokemon) => ({
